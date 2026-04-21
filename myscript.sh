4efc31a3bace814519f563f41d8c8c262eea12d1
Elizabeth Miramontes <elizabeth.miramontes@live.com>
2026-04-21 11:44:39 -0400

    Agrego correcciones de script

--- myscript.sh
+++ myscript.sh
@@ -1,25 +1,27 @@
 #! /bin/bash
 #script de prueba para kubernetes
 
-
 #creacion del usuario admin_user y configuracion de ssh para que pueda acceder sin contraseña al nodo master y worker.
-sudo adduser admin_user
+sudo adduser --disabled-password --gecos "" admin_user
 sudo usermod -aG sudo admin_user
 sudo mkdir -p /home/admin_user/.ssh
 sudo chmod 700 /home/admin_user/.ssh
 sudo touch /home/admin_user/.ssh/authorized_keys
-sudo nano /home/admin_user/.ssh/authorized_keys
-#Aqui tengo que ver como pegar la clave publica del usuario admin_user para que pueda acceder por ssh al nodo master y worker sin necesidad de contraseña.
+    #Opción A: Copiar un archivo que ya existe con la llave publica
+sudo install -m 600 -o admin_user -g admin_user /tmp/admin_user.pub /home/admin_user/.ssh/authorized_keys
+    #Opción B: pasar la llave como una variable
+#PUBKEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... comentario"
+#echo "$PUBKEY" | sudo tee -a /home/admin_user/.ssh/authorized_keys >/dev/null
 sudo chmod 600 /home/admin_user/.ssh/authorized_keys
 sudo chown -R admin_user:admin_user /home/admin_user/.ssh
 
 
 #Instalacion de containerd y configuracion del sistema para kubernetes.
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gpg
-sudo swapoff -a
-sudo nano /etc/fstab
-#Aqui tengo que comentar la linea del swap para que no se active al reiniciar el sistema, ya que kubernetes no funciona con swap activado.
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+sudo sed -i.bak '/\sswap\s/s/^/#/' /etc/fstab #hay que revisar
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -35,11 +37,13 @@ sudo sysctl --system
 sudo apt install -y containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
-sudo nano /etc/containerd/config.toml
-#Aqui tengo que Configurar containerd para usar systemd cgroups, en este archivo buscaremos "SystemdCgroup" y lo cambiaremo de false a true
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo systemctl enable containerd
-sudo systemctl status containerd #revissar si el comando de status es realmente necesario
+sudo systemctl is-active --quiet containerd || {
+  echo "containerd no está activo"
+  exit 1
+}
 sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -49,15 +53,13 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 sudo kubeadm init --pod-network-cidr=172.16.0.0/16
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.31.4/manifests/operator-crds.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.31.4/manifests/tigera-operator.yaml
 curl -O https://raw.githubusercontent.com/projectcalico/calico/v3.31.4/manifests/custom-resources.yaml
-nano custom-resources.yaml 
-#Aqui hay que validar que el rango de ips coincida o remediarlo
+#nano custom-resources.yaml  #Segun chatgpt calico hace los cambios por si mismo por lo cual este paso no es necesraio.
 kubectl create -f custom-resources.yaml
 kubectl taint nodes k8scp node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl create deployment nginx-demo --image=nginx
 kubectl expose deployment nginx-demo --type=NodePort --port=80 --target-port=80 --name=nginx-service
-#cambio jijitl
